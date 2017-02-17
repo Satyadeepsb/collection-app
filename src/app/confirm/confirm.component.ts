ef05e6d21a429d9440c55cbadee2c267dccaade5
Satyadeepsb <satyadeepsb@gmail.com>
2017-02-17 11:52:15 +0530

    working popup added

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+import {CollectableService} from "../shared/collectable.service";
+import {Collectable} from "../shared/collectable.model";
 
 @Component({
   selector: 'confirm',
@@ -7,12 +9,21 @@ import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
 })
 export class ConfirmComponent  extends DialogComponent {
 
-  constructor(dialogService: DialogService) {
+  collectionObject : Collectable ;
+
+  constructor(dialogService: DialogService,private collectableService : CollectableService) {
     super(dialogService);
+    this.collectionObject  = new Collectable('','');
+    if(this.collectableService.qq){
+      this.collectionObject= this.collectableService.qq;
+    }
   }
-  confirm() {
+  confirm(newCollection : Collectable) {
+
+    this.collectableService.newAdd(newCollection);
     // we set dialog result as true on click on confirm button,
     // then we can get dialog result from caller code
+    console.log('aaa');
     this.result = true;
     this.close();
   }
--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -45,6 +45,34 @@ export class MarketComponent implements OnInit {
 
   }
 
+  removeItem(item : Collectable){
+    this.collectableService.removeFromCollection(item);
+  }
+
+  editItem(item : Collectable){
+
+    let disposable = this.dialogService.addDialog(ConfirmComponent,
+      {
+      title: item.description,
+      message: item.type
+      })
+      .subscribe((isConfirmed)=>{
+        //We get dialog result
+        if(isConfirmed) {
+          alert('accepted');
+        }
+        else {
+          alert('declined');
+        }
+      });
+    //We can close dialog calling disposable.unsubscribe();
+    //If dialog was not closed manually close it by timeout
+    setTimeout(()=>{
+      disposable.unsubscribe();
+    },10000);
+  }
+
+
   showConfirm() {
     let disposable = this.dialogService.addDialog(ConfirmComponent, {
       title:'Confirm title',
